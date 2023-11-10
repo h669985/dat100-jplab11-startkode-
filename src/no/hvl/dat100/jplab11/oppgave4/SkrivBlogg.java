35b94a9152070ed60a6ad7fbcf1171468bc3ec54
h669985 <669985@stud.hvl.no>
2023-11-10 18:41:28 +0100

    leggTil() er skrevet litt mer robust nå siden den vil legge til på første ledige plass.

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,9 +1,7 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -11,14 +9,12 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-		File file = new File(mappe + filnavn); // Lager filen
-		String filtekst = samling.toString(); // Lager filteksten
 
-		try (PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8)) { //Lager en filskriver
-			writer.print(filtekst); // Skrivet til filen
+		try (PrintWriter writer = new PrintWriter(mappe + filnavn)) {
+			writer.print(samling.toString());
 			return true;
 
-		} catch (IOException e) { //Mer generic error handling
+		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			return false;
 		}
